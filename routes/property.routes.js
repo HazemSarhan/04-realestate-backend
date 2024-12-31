be4363114e95e59370bc18e9dd0bbb1dcb443bc7
amd200 <72322072+amd200@users.noreply.github.com>
2024-12-31 18:27:36 +0200

    Update property.routes.js

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -1,26 +1,10 @@
-import express from 'express';
-import {
-  createProperty,
-  getAllProperties,
-  getPropertyById,
-  updateProperty,
-  deleteProperty,
-} from '../controllers/property.controller.js';
-import {
-  authenticatedUser,
-  authorizePermissions,
-} from '../middleware/authentication.js';
+import express from "express";
+import { createProperty, getAllProperties, getPropertyById, updateProperty, deleteProperty } from "../controllers/property.controller.js";
+import { authenticatedUser, authorizePermissions } from "../middleware/authentication.js";
 const router = express.Router();
 
-router
-  .route('/')
-  .post([authenticatedUser], createProperty)
-  .get([authenticatedUser], getAllProperties);
+router.route("/").post([authenticatedUser, authorizePermissions("ADMIN")] , createProperty).get(getAllProperties);
 
-router
-  .route('/:id')
-  .get([authenticatedUser], getPropertyById)
-  .patch([authenticatedUser], updateProperty)
-  .delete([authenticatedUser], deleteProperty);
+router.route("/:id").get(getPropertyById).patch([authenticatedUser], updateProperty).delete([authenticatedUser], deleteProperty);
 
 export default router;
